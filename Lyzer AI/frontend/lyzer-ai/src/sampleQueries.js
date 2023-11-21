0efaa759bf682153cb9d555f0a6c5d92bbb1c074
abdulrehman <abdulrehman00396@gmail.com>
2023-11-21 23:20:09 +0500

    updated questions in sampleQueries.js

--- Lyzer AI/frontend/lyzer-ai/src/sampleQueries.js
+++ Lyzer AI/frontend/lyzer-ai/src/sampleQueries.js
@@ -8,8 +8,8 @@ const samples=[
         answer:"You can use for free or buy subscription to access some awesome features."
     },
     {
-        question:"What is Lyzer-AI?",
-        answer:"It is a tool that answers your queries."
+        question:"Core purpose of Lyzer-AI?",
+        answer:"To enable our clients in making their own chatbot for their websites."
     },
     {
         question:"How can I use Lyzer-AI?",
